8b5af8b3e893161fb70f0d609d824bf7c49db52b
tomtatyrek <78051241+tomtatyrek@users.noreply.github.com>
2025-04-13 08:28:14 +0200

    Added JavaDoc and a license

--- lib/src/main/java/io/github/tomtatyrek/jlogger/JLogger.java
+++ lib/src/main/java/io/github/tomtatyrek/jlogger/JLogger.java
@@ -5,19 +5,35 @@ import java.nio.file.FileSystems;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * An instance of this class is used for simple logging to the console and/or to a file.
+ *
+ * @version 1.0.0
+ * @author Tomáš Tatyrek
+ */
 public class JLogger {
 
     //====STATIC CONSTANTS====
-    public static final String DEFAULT_LOG_DTF_PATTERN = "HH:mm:ss.SSS";
-    public static final String DEFAULT_FILENAME_DTF_PATTERN = "yyyy_MM_dd-HH_mm_ss";
+    private static final String DEFAULT_LOG_DTF_PATTERN = "HH:mm:ss.SSS";
+    private static final String DEFAULT_FILENAME_DTF_PATTERN = "yyyy_MM_dd-HH_mm_ss";
 
     //====PRIVATE FIELDS====
     private PrintStream consoleOutputStream = null;
     private BufferedWriter fileWriter = null;
-    private DateTimeFormatter logDTF;
+    private final DateTimeFormatter logDTF;
     private DateTimeFormatter filenameDTF = null;
 
     //====CONSTRUCTORS====
+
+    /**
+     * Returns a JLogger object, which logs to console and to a file, using specified {@link java.time.format.DateTimeFormatter DateTimeFormatter} patterns.
+     *
+     * @param logPath A string path to your log file <b>OR</b> the folder in which you want to keep automatically created log files.
+     * @param filenameDTFPattern A {@link java.time.format.DateTimeFormatter DateTimeFormatter} string pattern for the format of date and time in the automatically created log files.
+     * @param consoleStream A {@link java.io.PrintStream PrintStream} to which console log messages will be printed <i>e.g. System.out</i>
+     * @param logDTFPattern A {@link java.time.format.DateTimeFormatter DateTimeFormatter} string pattern for the format of date and time shown in the log messages.
+     * @throws IOException if there was an error while creating or wtiting to the specified log file or log folder
+     */
     public JLogger(String logPath, String filenameDTFPattern, PrintStream consoleStream, String logDTFPattern) throws IOException {
 
         //Initializes log DateTimeFormater field
@@ -30,6 +46,14 @@ public class JLogger {
 
     }
 
+    /**
+     * Returns a JLogger object, which logs only to a file, using specified {@link java.time.format.DateTimeFormatter DateTimeFormatter} patterns.
+     *
+     * @param logPath A string path to your log file <b>OR</b> the folder in which you want to keep automatically created log files.
+     * @param filenameDTFPattern A {@link java.time.format.DateTimeFormatter DateTimeFormatter} string pattern for the format of date and time in the automatically created log files.
+     * @param logDTFPattern A {@link java.time.format.DateTimeFormatter DateTimeFormatter} string pattern for the format of date and time shown in the log messages.
+     * @throws IOException if there was an error while creating or wtiting to the specified log file or log folder
+     */
     public JLogger(String logPath, String filenameDTFPattern, String logDTFPattern) throws IOException {
 
         //Initializes log DateTimeFormater field
@@ -40,6 +64,30 @@ public class JLogger {
 
     }
 
+    /**
+     * Returns a JLogger object, which logs only to a file, using specified {@link java.time.format.DateTimeFormatter DateTimeFormatter} pattern in log messages and the default {@link java.time.format.DateTimeFormatter DateTimeFormatter} pattern in file names.
+     *
+     * @param logPath A string path to your log file <b>OR</b> the folder in which you want to keep automatically created log files.
+     * @param logDTFPattern A {@link java.time.format.DateTimeFormatter DateTimeFormatter} string pattern for the format of date and time shown in the log messages.
+     * @throws IOException if there was an error while creating or wtiting to the specified log file or log folder
+     */
+    public JLogger(String logPath, String logDTFPattern) throws IOException {
+
+        //Initializes log DateTimeFormater field
+        logDTF = DateTimeFormatter.ofPattern(logDTFPattern);
+        filenameDTF = DateTimeFormatter.ofPattern(DEFAULT_FILENAME_DTF_PATTERN);
+
+        setupFileWriter(logPath);
+
+    }
+
+    /**
+     * Returns a JLogger object, which logs to console and to a file, using default {@link java.time.format.DateTimeFormatter DateTimeFormatter} pattern.
+     *
+     * @param logPath A string path to your log file <b>OR</b> the folder in which you want to keep automatically created log files.
+     * @param consoleStream A {@link java.io.PrintStream PrintStream} to which console log messages will be printed <i>e.g. System.out</i>
+     * @throws IOException if there was an error while creating or wtiting to the specified log file or log folder
+     */
     public JLogger(String logPath, PrintStream consoleStream) throws IOException {
 
         //Initializes log DateTimeFormater field
@@ -52,6 +100,12 @@ public class JLogger {
 
     }
 
+    /**
+     * Returns a JLogger object, which logs only to a file, using default {@link java.time.format.DateTimeFormatter DateTimeFormatter} patterns.
+     *
+     * @param logPath A string path to your log file <b>OR</b> the folder in which you want to keep automatically created log files.
+     * @throws IOException if there was an error while creating or wtiting to the specified log file or log folder
+     */
     public JLogger(String logPath) throws IOException {
 
         //Initializes log DateTimeFormater field
@@ -62,6 +116,12 @@ public class JLogger {
 
     }
 
+    /**
+     * Returns a JLogger object, which logs only to a console, using specified {@link java.time.format.DateTimeFormatter DateTimeFormatter} pattern.
+     *
+     * @param consoleStream A {@link java.io.PrintStream PrintStream} to which console log messages will be printed <i>e.g. System.out</i>
+     * @param logDTFPattern A {@link java.time.format.DateTimeFormatter DateTimeFormatter} string pattern for the format of date and time shown in the log messages.
+     */
     public JLogger(PrintStream consoleStream, String logDTFPattern) {
 
         logDTF = DateTimeFormatter.ofPattern(logDTFPattern);
@@ -69,6 +129,11 @@ public class JLogger {
 
     }
 
+    /**
+     * Returns a JLogger object, which logs only to a console, using default {@link java.time.format.DateTimeFormatter DateTimeFormatter} pattern.
+     *
+     * @param consoleStream A {@link java.io.PrintStream PrintStream} to which console log messages will be printed <i>e.g. System.out</i>
+     */
     public JLogger(PrintStream consoleStream) {
 
         logDTF = DateTimeFormatter.ofPattern(DEFAULT_LOG_DTF_PATTERN);
@@ -76,26 +141,65 @@ public class JLogger {
 
     }
 
+    /**
+     * Returns default JLogger object, which logs only to <i>System.out</i>
+     */
     public JLogger() {
         logDTF = DateTimeFormatter.ofPattern(JLogger.DEFAULT_LOG_DTF_PATTERN);
         consoleOutputStream = System.out;
     }
     //====PUBLIC METHODS====
+
+    /**
+     * Logs a <b>TRACE</b> message.
+     *
+     * @param message A String message to be logged.
+     */
     public void trace(String message) {
         logSpecifiedType("TRACE", message);
     }
+
+    /**
+     * Logs a <b>DEBUG</b> message.
+     *
+     * @param message A String message to be logged.
+     */
     public void debug(String message) {
         logSpecifiedType("DEBUG", message);
     }
+
+    /**
+     * Logs an <b>INFO</b> message.
+     *
+     * @param message A String message to be logged.
+     */
     public void info(String message) {
         logSpecifiedType("INFO", message);
     }
+
+    /**
+     * Logs a <b>WARN</b> message.
+     *
+     * @param message A String message to be logged.
+     */
     public void warn(String message) {
         logSpecifiedType("WARN", message);
     }
+
+    /**
+     * Logs an <b>ERROR</b> message.
+     *
+     * @param message A String message to be logged.
+     */
     public void error(String message) {
         logSpecifiedType("ERROR", message);
     }
+
+    /**
+     * Logs a <b>FATAL</b> message.
+     *
+     * @param message A String message to be logged.
+     */
     public void fatal(String message) {
         logSpecifiedType("FATAL", message);
     }
